79bdb3e1122ad595762365050b2f1ec3b5071f95
Mr. Devansh Shah <devansh.shah.de@gmail.com>
2025-11-20 19:47:12 +0530

    Assignment Operator Practise

---
+++ py_fundamentals/variables.py
@@ -0,0 +1,19 @@
+a = 1
+b = 10
+result = a == b
+print(result)  # Output: True
+
+x = 10
+print(x)  # Output: 10
+
+x += 5
+print(x)  # Output: 15
+
+x -= 5
+print(x)  # Output: 10
+
+x /= 5
+print(x)  # Output: 2.0
+
+x *= 5
+print(x)  # Output: 10.0
